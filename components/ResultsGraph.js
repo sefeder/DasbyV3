eb71a37e1cf71ad802e878968487db39c702c754
Seth Feder <sethfeder2017@u.northwestern.edu>
2019-05-06 20:57:07 -0500

    minor edits to results graph using react-native-svg-charts

--- components/ResultsGraph.js
+++ components/ResultsGraph.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 import moment from 'moment';
 import 'moment-timezone';
 import { LineChart, Grid, XAxis, YAxis } from 'react-native-svg-charts'
+import * as scale from 'd3-scale'
 
 export default class ResultsGraph extends Component {
 
@@ -36,24 +37,34 @@ export default class ResultsGraph extends Component {
 
     render() {
         const lineChartData = this.props.dataArray.map(dataPoint=>dataPoint.severity)
+        console.log('lineChartData: ', lineChartData)
         const axesSvg = { fontSize: 15, fill: 'grey' };
         const verticalContentInset = { top: 7, bottom: 7 }
         const xAxisHeight = 30
+        // const yRange = scale.scaleLinear([0, 100], [0, 100]);
         return (
             <View style={{flexDirection: 'row', height: 440}}>
                 <YAxis
-                    data={lineChartData}
+                    data={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
                     style={{ paddingBottom: xAxisHeight, height: 400, backgroundColor: 'white'}}
                     contentInset={verticalContentInset}
                     svg={axesSvg}
+                    formatLabel={value=>`${value}`}
+                    // numberOfTicks={10}
+                    // ticks={[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
                 />
                 <ScrollView style={{height:400}} horizontal>
                     <View style={styles.container}>
                         <LineChart
-                            style={{height: 400, width: 1000}}
+                            style={{height: 400, width: 500}}
                             data={lineChartData}
                             svg={{ stroke: 'rgba(134, 65, 244, 1)', strokeWidth: 3}}
-                            contentInset={{top: 20, bottom: 20, left: 5, right: 5}}
+                            contentInset={{top: 40, bottom: 40, left: 10, right: 10}}
+                            // yScale={scale.scaleLinear}
+                            yMin={0}
+                            yMax={100}
+                            // numberOfTicks={10}
+                            
                         >
                             <Grid/>
                         </LineChart>
